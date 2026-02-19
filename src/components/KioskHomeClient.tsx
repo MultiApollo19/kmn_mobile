8362395429ec62f4a77be9bcce31c750c8fc1e76
MultiApollo19 <mariusz.belwon@gmail.com>
2026-02-19 13:00:53 +0100

    Kiosk update

--- src/components/KioskHomeClient.tsx
+++ src/components/KioskHomeClient.tsx
@@ -535,7 +535,7 @@ export default function KioskHomeClient({
                 </div>
               </div>
 
-              <div className="p-4 flex-1 min-h-0 overflow-y-auto">
+              <div className="p-4 flex-1 min-h-0 overflow-hidden">
                 <form onSubmit={handleAdmission} className="space-y-4">
                   
                   {/* Visitor Name */}
@@ -667,7 +667,7 @@ export default function KioskHomeClient({
               </div>
             </div>
 
-            <div className="flex-1 min-h-0 overflow-y-auto pr-1">
+            <div className="kiosk-scrollbar flex-1 min-h-0 overflow-y-scroll pr-1 overscroll-contain">
               {loadingVisits ? (
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   {[1,2,3,4].map(i => (
